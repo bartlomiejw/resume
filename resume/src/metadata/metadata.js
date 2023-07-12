7b7e073fbd0f7f2bd2e63df14d372618999eee6f
Bartłomiej Warias <bartlomiej.warias@applover.pl>
2023-07-13 01:12:26 +0200

    first commit

---
+++ resume/src/metadata/metadata.js
@@ -0,0 +1,125 @@
+module.exports = {
+  name: 'Bartłomiej Warias',
+  title: 'Frontend Developer',
+  facts: {
+    'Github': `<a href="https://github.com/bartlomiejw">
+                  <i class="fa fa-brands fa-github"></i>github.com/bartlomiejw
+                </a>`,
+    'Linkedin': `<a href="https://www.linkedin.com/in/bwarias/"><i class="fa fa-brands fa-linkedin-in"></i>linkedin.com/in/bwarias/</a>`,
+    'Email': '<a href="mailto:wariasbj@gmail.com"><i class="fa fa-envelope fact-icon"></i>wariasbj@gmail.com</a>'
+  },
+  skills: [
+    ['Javascript', 100],
+    ['Typescript', 100],
+    ['ECMAScript 6', 100],
+    ['HTML', 100],
+    ['CSS / SCSS', 100],
+    ['Wordpress', 100],
+    ['Sage Wordpress Theme Starter', 100],
+    ['Blade template', 100],
+    ['Webpack', 100],
+    ['Vite', 100],
+    ['Vue.js 3', 100],
+    ['Responsive Design', 100],
+    ['GIT', 100],
+    ['Metodologia BEM', 100],
+    ['Figma', 100],
+    ['Docker', 100],
+    ['Caprover', 100],
+    ['MySQL', 100],
+    ['Graphql', 100],
+    ['REST API', 100],
+    ['Język angielski na poziomie B2', 100]
+  ],
+  positions: [
+    {
+      title: 'BigCom',
+      period: 'Luty, 2011 - Maj, 2011',
+      skills: ['Junior Webdeveloper', 'HTML', 'CSS', 'JS', 'PHP'],
+      contents: `
+Kodowanie szablonów graficznych stron www w oparciu o projekt Adobe Photoshop, stosowanie RWD, implementacja do środowiska Wordpress. Kodowanie funkcjonalności dla Wordpress, edycja wtyczek Wordpress na potrzeby szablonów, obróbka plików graficznych w programie Adobe Photoshop. `
+    },
+    {
+      title: 'Grupa WW Sp. z o.o.',
+      period: 'czerwiec, 2018 - wrzesień, 2021',
+      skills: ['Webdeveloper', 'HTML', 'CSS / SCSS', 'JS', 'PHP', 'Webpack', 'Sage Starter Theme'],
+      contents: `
+Kodowanie dedykowanych szablonów graficznych na bazie projektów figma / .PSD. Przygotowywanie środowiska, repozytorium projektu. Przygotowywanie hostingu - konfiguracja domeny, itp. Wycena projektów, przygotowywanie logiki dedykowanego szablonu (współpraca z UX/UI Designer). Współpraca z klientami takimi jak Asseco Poland, Olimp Labs czy Petrosoft. Optymalizacja witryn pod kątem pagespeed. `
+    },
+    {
+      title: 'Applover Full-Stack Digital Agency',
+      period: 'październik 2021 - obecnie',
+      skills: ['Frontend developer', 'HTML', 'CSS / SCSS', 'JS', 'PHP', 'Webpack', 'Sage Starter Theme', 'VUE.js', 'Typescript', 'Docker', 'Strapi', ''],
+      contents: `
+In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
+`
+    }
+  ],
+  experience: [
+    {
+      header: 'Strona internetowa promująca kampanię firmy z branży IT',
+      skills: ['html', 'css / scss', 'php', 'sage wordpress theme', 'js', 'blade template', 'webpack', 'acf'],
+      contents: `
+Motyw WordPress, który tworzę, obejmuje szeroki zakres funkcjonalności i integracji, które wzbogacają witrynę i zapewniają użytkownikom wyjątkowe doświadczenia.
+Integracja z zewnętrzną aplikacją clickmeeting.com umożliwia organizowanie profesjonalnych spotkań online, webinarów i szkoleń bezpośrednio na stronie. Dzięki temu użytkownicy mogą łatwo zarejestrować się na wydarzenia, uczestniczyć w nich i korzystać z interaktywnych funkcji udostępnianych przez clickmeeting.com.
+Blog i video-blog to kluczowe elementy, które umożliwiają publikację treści w postaci artykułów lub wideo. Ta funkcjonalność pozwala na tworzenie interesujących i angażujących wpisów, które mogą przyciągnąć uwagę czytelników i zachęcić do interakcji.
+Formularz kontaktowy to ważny element, który umożliwia użytkownikom skontaktowanie się z właścicielem witryny. Dzięki niemu użytkownicy mogą wysłać wiadomość bezpośrednio z witryny, co ułatwia nawiązanie komunikacji i zwiększa zaangażowanie.
+Niestandardowy design to element, który wyróżnia ten motyw od innych. Odpowiednio zaprojektowany interfejs, dopasowany do marki i celów biznesowych, zapewnia estetyczny wygląd i pozytywne wrażenia użytkowników.
+Wszystkie te funkcje są starannie zintegrowane, aby stworzyć kompleksowy motyw WordPress, który spełnia potrzeby biznesowe i dostarcza wartościową zawartość dla użytkowników. Profesjonalny wygląd, łatwość obsługi i wysoka funkcjonalność to cechy, które wyróżniają ten motyw od konkurencji i przyciągają uwagę odwiedzających.`
+    },
+    {
+      header: 'Platforma szkoleniowa z systemem rejestracji oraz logowania dla userów',
+      skills: ['html', 'css / scss', 'php', 'sage wordpress theme', 'js', 'blade template', 'webpack', 'acf'],
+      contents: `
+
+Motyw WordPress oparty na sage wordpress starter theme zapewnia funkcje rejestracji użytkowników oraz dostęp do treści dla zalogowanych użytkowników. Dzięki wbudowanym formularzom kontaktowym, użytkownicy mogą z łatwością zapisywać się na szkolenia. Dodatkowo, istnieje system punktacji, który nagradza użytkowników za różne zdarzenia, a zebrane punkty mogą być wymieniane na nagrody.
+
+Ten zaawansowany motyw WordPress oferuje profesjonalne rozwiązania do zarządzania rejestracjami użytkowników, dostępem do treści i interakcjami na stronie. Dzięki wykorzystaniu sage wordpress starter theme, motyw jest dobrze zorganizowany i łatwy do konfiguracji. Jest to idealne narzędzie dla platform szkoleniowych lub stron, które chcą zapewnić spersonalizowane doświadczenie dla swoich użytkowników poprzez system rejestracji, treści dostępne tylko dla zalogowanych oraz możliwość zdobywania i wymieniania punktów na nagrody.`
+    },
+    {
+      header: 'Strona internetowa dla software house',
+      skills: ['html', 'css / scss', 'php', 'custom wordpress starter theme', 'js', 'webpack', 'acf'],
+      contents: `Motyw WordPress, który opracowałem, został oparty na moim własnym starterowym motywie z wykorzystaniem narzędzia Webpack. Strona internetowa zawiera różnorodne sekcje, takie jak blog, case study oraz sekcję z ofertami pracy (careers), która została zintegrowana z zewnętrzną aplikacją do publikowania ofert pracy.
+Podjęto również działania mające na celu optymalizację istniejących funkcjonalności strony, aby zapewnić lepszą wydajność i szybkość ładowania. System zarządzania treścią (CMS) jest wyposażony w niestandardowe szablony, które umożliwiają łatwe wprowadzanie treści na stronę. Te szablony zostały przygotowane z wykorzystaniem wtyczki ACF (Advanced Custom Fields), co pozwala na dostosowanie zawartości strony do indywidualnych potrzeb.
+Dzięki tym rozwiązaniom, motyw WordPress zapewnia profesjonalny wygląd, wydajność, elastyczność i łatwość użytkowania. Jest to kompleksowe rozwiązanie, które umożliwia efektywne zarządzanie treściami na stronie oraz zapewnia pozytywne doświadczenia dla odwiedzających.`
+    },
+    {
+      header: 'Strona internetowa poświęcona promocji art NFT',
+      skills: ['html', 'css / scss', 'php', 'custom wordpress starter theme', 'js', 'webpack', 'acf'],
+      contents: `Opublikowany przeze mnie motyw WordPress opiera się na spersonalizowanym starterowym motywie, który został rozwinięty przy użyciu narzędzia Webpack. Motyw zawiera integrację z platformą Coinbase.com, która umożliwia pobieranie danych dotyczących NFT (non-fungible token) na podstawie podanego identyfikatora oraz wyświetlanie aktualnego kursu NFT.
+Ta integracja pozwala użytkownikom na łatwe uzyskanie informacji o konkretnym NFT, takich jak jego wartość rynkowa, statystyki i inne szczegóły. Dzięki temu motyw oferuje unikalne możliwości związane z obsługą i wyświetlaniem informacji na temat NFT na stronie WordPress.
+Motyw ten zapewnia profesjonalny wygląd, wydajność i elastyczność, umożliwiając łatwe zarządzanie treściami na stronie WordPress oraz dostarczając użytkownikom cennych danych na temat NFT z platformy Coinbase.com.`
+    },
+    {
+      header: 'Wtyczka wyświetlająca aktualne kursy walut',
+      skills: ['html', 'css / scss', 'php', 'custom wordpress starter plugin', 'js', 'vite', 'vue', 'typescript'],
+      contents: `Wtyczka oparta na frameworku Vue.js i zintegrowana z silnikiem WordPress umożliwia łatwe pobieranie aktualnych kursów walut z api Narodowego Banku Polskiego. Dzięki tej wtyczce użytkownicy mogą skonfigurować wygląd widoku oraz wybrać, które waluty mają być wyświetlane.
+Ta zaawansowana wtyczka umożliwia dostęp do aktualnych danych walutowych, co jest przydatne dla osób, które śledzą kursy walut na bieżąco lub potrzebują ich w celach biznesowych. Użytkownicy mogą dostosować wygląd widoku wtyczki, aby pasował do ich indywidualnych preferencji, a także wybrać, które waluty mają być wyświetlane.
+Wtyczka ta zapewnia profesjonalne rozwiązanie do obsługi kursów walut w WordPress, łącząc zalety frameworku Vue.js z potężnym silnikiem WordPress. Jest to narzędzie, które ułatwia monitorowanie i wyświetlanie aktualnych kursów walut na stronie internetowej.`
+    },
+    {
+      header: 'Wtyczka wyświetlajca na stronie kalkulator kredytów' ,
+      skills: ['html', 'css / scss', 'php', 'custom wordpress starter plugin', 'js', 'vite', 'vue', 'typescript'],
+      contents: `Wtyczka oparta na frameworku Vue.js i zintegrowana z silnikiem WordPress oferuje funkcjonalności kalkulatora kredytowego oraz formularza kontaktowego. Kalkulator kredytów jest w pełni konfigurowalny z poziomu panelu administracyjnego, umożliwiając dostosowanie go do indywidualnych potrzeb. Użytkownicy mogą wprowadzać dane dotyczące kredytu, a wtyczka automatycznie generuje odpowiednie obliczenia i wyświetla wyniki.
+Dodatkowo, wtyczka umożliwia wysyłanie wiadomości e-mail za pomocą formularza kontaktowego. Domyślnie ustawiona wiadomość zawiera wszystkie informacje o kredycie wprowadzone przez użytkownika, co ułatwia komunikację i przekazanie istotnych danych.
+Ta zaawansowana wtyczka zapewnia profesjonalne narzędzia do obsługi kalkulatora kredytowego i formularza kontaktowego w WordPress. Integracja z frameworkiem Vue.js pozwala na płynne i responsywne interakcje, a połączenie z silnikiem WordPress daje możliwość łatwego zarządzania treściami i konfiguracji wtyczki.`
+    },
+    {
+      header: 'Web aplikacja do zarządzania pracownikami w firmie' ,
+      skills: ['html', 'css / scss', 'js', 'vite', 'vue', 'typescript', 'element-plus'],
+      contents: `Web aplikacja oparta na Vue.js umożliwiająca zarządzanie i wyświetlanie informacji dotyczących pracy. Dostępna funkcjonalność logowania umożliwia korzystanie z różnych typów kont, które wyświetlają dane i umożliwiają zarządzanie ankietami ewaluacyjnymi. Aplikacja wykorzystuje biblioteki JS, takie jak chart.js i moment.js, aby zapewnić zaawansowane funkcje, takie jak wykresy i manipulacje z datami.
+Ta profesjonalna aplikacja oparta na Vue.js zapewnia intuicyjne narzędzia do zarządzania danymi i realizacji ankiet ewaluacyjnych. Dzięki wykorzystaniu różnych bibliotek JS, aplikacja oferuje zaawansowane funkcje wizualizacji danych i łatwe manipulacje z datami. To doskonałe narzędzie dla organizacji, które potrzebują skutecznego sposobu zarządzania informacjami dotyczącymi pracy i przeprowadzania ankiet ewaluacyjnych.`
+    },
+    {
+      header: 'Portal bankowy informujący o aktualnych wydarzeniach oraz wyświetlający dane pobierane z API banku' ,
+      skills: ['html', 'css / scss', 'php', 'custom wordpress starter theme', 'js', 'webpack', 'acf'],
+      contents: `
+Opracowałem niestandardowy motyw dla banku, który oferuje wiele zaawansowanych funkcji. Jedną z głównych funkcjonalności jest automatyczne pobieranie codziennych danych z API i zapisywanie ich w bazie danych za pomocą CRON. Motyw zawiera również moduł czyszczenia cache'u po zakończeniu operacji. Na stronie wykorzystano bibliotekę highcharts do prezentacji danych na interaktywnych wykresach. Dodatkowo, zaprojektowana została wyszukiwarka dla Mega menu, umożliwiająca szybkie wyszukiwanie elementów menu. Dla ułatwienia edycji treści, motyw posiada szereg bloków dla edytora Gutenberg. Strona została zintegrowana z Varnish przy użyciu wtyczki WP Rocket, zapewniającej efektywne zarządzanie cache'em. W motywie zostały także przygotowane customowe szablony do wyświetlania treści w sposób spersonalizowany i atrakcyjny dla użytkowników.`
+    },
+    {
+      header: 'Web aplikacja wraz z narzędziem do analizy emocjonalnej młodych ludzi' ,
+      skills: ['html', 'css / scss', 'js', 'vite', 'vue', 'typescript', 'element-plus', 'strapi', 'hetzner cloud', 'docker', 'docker-compose', 'caprover'],
+      contents: `Web aplikacja oparta na Vue.js umożliwiająca wykonywanie testu po rejestracji i zalogowaniu się. Test daje możliwość wyświetlenia feedbacku oraz raportu do pobrania w formacie .pdf. Aplikacja posiada 3 typy kont, blog, panel użytkownika oraz zarządzanie treścią za pomocą headless cms Strapi`
+    },
+  ]
+};
